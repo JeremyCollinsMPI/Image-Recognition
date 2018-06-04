723b6192e9a80abf5ba9d64e2a6321ac0dc1d486
Jeremy Collins <jeremyccollins@gmail.com>
2018-06-04 14:21:24 +0800

    Added experiment1.py

--- experiment1.py
+++ experiment1.py
@@ -1,17 +1,4 @@
-#  Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-"""Convolutional Neural Network Estimator for MNIST, built with tf.layers."""
+# Bayesian image classifier, which classifies images from MNIST with 10 images per class as training data 
 
 from __future__ import absolute_import
 from __future__ import division
@@ -23,6 +10,7 @@ import random
 import scipy
 tf.logging.set_verbosity(tf.logging.INFO)
 
+# set random seed
 random.seed(13)
 
 def copy(x):
@@ -41,99 +29,6 @@ def replist(x,y):
 	for m in xrange(y):
 		new.append(copy(x))
 	return new
-	
-def cnn_model_fn(features, labels, mode):
-  """Model function for CNN."""
-  # Input Layer
-  # Reshape X to 4-D tensor: [batch_size, width, height, channels]
-  # MNIST images are 28x28 pixels, and have one color channel
-  input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
-  tf.print(input_layer)
-  # Convolutional Layer #1
-  # Computes 32 features using a 5x5 filter with ReLU activation.
-  # Padding is added to preserve width and height.
-  # Input Tensor Shape: [batch_size, 28, 28, 1]
-  # Output Tensor Shape: [batch_size, 28, 28, 32]
-  conv1 = tf.layers.conv2d(
-      inputs=input_layer,
-      filters=32,
-      kernel_size=[5, 5],
-      padding="same",
-      activation=tf.nn.relu)
-
-  # Pooling Layer #1
-  # First max pooling layer with a 2x2 filter and stride of 2
-  # Input Tensor Shape: [batch_size, 28, 28, 32]
-  # Output Tensor Shape: [batch_size, 14, 14, 32]
-  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
-
-  # Convolutional Layer #2
-  # Computes 64 features using a 5x5 filter.
-  # Padding is added to preserve width and height.
-  # Input Tensor Shape: [batch_size, 14, 14, 32]
-  # Output Tensor Shape: [batch_size, 14, 14, 64]
-  conv2 = tf.layers.conv2d(
-      inputs=pool1,
-      filters=64,
-      kernel_size=[5, 5],
-      padding="same",
-      activation=tf.nn.relu)
-
-  # Pooling Layer #2
-  # Second max pooling layer with a 2x2 filter and stride of 2
-  # Input Tensor Shape: [batch_size, 14, 14, 64]
-  # Output Tensor Shape: [batch_size, 7, 7, 64]
-  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-
-  # Flatten tensor into a batch of vectors
-  # Input Tensor Shape: [batch_size, 7, 7, 64]
-  # Output Tensor Shape: [batch_size, 7 * 7 * 64]
-  pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
-
-  # Dense Layer
-  # Densely connected layer with 1024 neurons
-  # Input Tensor Shape: [batch_size, 7 * 7 * 64]
-  # Output Tensor Shape: [batch_size, 1024]
-  dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
-
-  # Add dropout operation; 0.6 probability that element will be kept
-  dropout = tf.layers.dropout(
-      inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
-
-  # Logits layer
-  # Input Tensor Shape: [batch_size, 1024]
-  # Output Tensor Shape: [batch_size, 10]
-  logits = tf.layers.dense(inputs=dropout, units=10)
-
-  predictions = {
-      # Generate predictions (for PREDICT and EVAL mode)
-      "classes": tf.argmax(input=logits, axis=1),
-      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-      # `logging_hook`.
-      "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-  }
-  if mode == tf.estimator.ModeKeys.PREDICT:
-    return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
-
-  # Calculate Loss (for both TRAIN and EVAL modes)
-  loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-
-  # Configure the Training Op (for TRAIN mode)
-  if mode == tf.estimator.ModeKeys.TRAIN:
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-    train_op = optimizer.minimize(
-        loss=loss,
-        global_step=tf.train.get_global_step())
-    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
-  # Add evaluation metrics (for EVAL mode)
-  eval_metric_ops = {
-      "accuracy": tf.metrics.accuracy(
-          labels=labels, predictions=predictions["classes"])}
-  return tf.estimator.EstimatorSpec(
-      mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
-
-
 
 	
 
@@ -144,22 +39,7 @@ def main(unused_argv):
   
   n = 24
 
-  train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
-#   m = train_data[sevens[0]]
-#   for n in range(1, 4):
-#   	print(n)
-#   	m = np.add(m, train_data[sevens[n]])
-#   print(m)
-#   m = np.divide(m, len(sevens))
-#   print(m)
-# 
-#  
- 
-  first = train_data[n]
-#   first = [1 - x for x in first]
-  img = Image.new('1',(28,28))
-  img.putdata(first)
-  
+  train_labels = np.asarray(mnist.train.labels, dtype=np.int32)  
   
   def show(number):
   	x = train_data[number]
@@ -168,24 +48,6 @@ def main(unused_argv):
   	img.putdata(x)
   	img.show()
 
-#   img.show()
-  
-#   def findError(imageNumber1, imageNumber2):
-#   	first = train_data[imageNumber1]
-#   	second = train_data[imageNumber2]
-#   	result = np.absolute(np.subtract(first, second))
-#   	result = [1 - (0.95*x) for x in result]
-#   	logResult = [np.log(m) for m in result]
-#   	print(logResult)
-#   	print(np.sum(logResult))
-#   def prepareImage(imageNumber):
-#   
-#   	first = train_data[imageNumber]
-#   	first = np.ndarray.flatten(first)
-#   	img = Image.new('1',(28,28))
-#   	img.putdata(first)
-#   	first = np.reshape(img, 784)
-#   	return img
 
   def findError(imageNumber1, imageNumber2, a, b):
   	first = train_data[imageNumber1]
@@ -328,12 +190,6 @@ def main(unused_argv):
 			y = tryAgainstRandomExample(imageNumber1, x)
 			y2 = tryAgainstRandomExample2(imageNumber1, x)
 			y = max(y, y2)
-# 			y = y + numericalStability
-# 			y = np.exp(y)
-# 			total = total + y
-# 		total = total / trainingExamples
-# 		total = np.log(total)
-# 		total = total - numericalStability
 			intermediateResults.append(y)
 		results.append(max(intermediateResults))
 	return results.index(max(results)), results
@@ -353,86 +209,17 @@ def main(unused_argv):
 		total = total - numericalStability
   		results.append(total)
   	return results.index(max(results)), results
-  	
+
+# example image and classification  	
   print(classify1and2(252,3)[0])
-  	
-#   result = []
-#   for i in range(350,370):
-#   	result.append(train_labels[i]==classify1and2(i, 10)[0])
-#   	print(len(result))
-#   	print(len([x for x in result if x == True])/len(result))
-#   print(len([x for x in result if x == True])/len(result))
 
-  def produce(array, list):
-  	for member in list:
-  		x = member[0]
-  		y = member[1]
-  		xContinue = member[2]
-  		yContinue = member[3]
-  		for m in xrange(xContinue):
-  			array[y][x+m] = 1
-  		for m in xrange(yContinue):
-  			array[y+m][x] = 1
-  	return array
-  
-#   def model(array, numberOfComponents):
-  	
-#   print(produce(replist(rep(0,28), 28), [[0,1,2,1]]))
-  		
-
-	  
-	  
-	  
-	  
-# 	  def showClass(number):
-# 		sevens = [x for x in xrange(len(train_labels)) if train_labels[x] == number]
-# 		print(sevens)
-# 	showClass(4)
-
-  
-  
-#   findError(24,44, 3, 3)
-  	
-
-  
-#   print(train_labels[n])
-#   print(len(train_labels))
-  
-#   img.save('my.png')
-  
-#   eval_data = mnist.test.images  # Returns np.array
-#   eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
-
-  # Create the Estimator
-#   mnist_classifier = tf.estimator.Estimator(
-#       model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
-# 
-#   # Set up logging for predictions
-#   # Log the values in the "Softmax" tensor with label "probabilities"
-#   tensors_to_log = {"probabilities": "softmax_tensor"}
-#   logging_hook = tf.train.LoggingTensorHook(
-#       tensors=tensors_to_log, every_n_iter=50)
-# 
-#   # Train the model
-#   train_input_fn = tf.estimator.inputs.numpy_input_fn(
-#       x={"x": train_data},
-#       y=train_labels,
-#       batch_size=100,
-#       num_epochs=None,
-#       shuffle=True)
-#   mnist_classifier.train(
-#       input_fn=train_input_fn,
-#       steps=20000,
-#       hooks=[logging_hook])
-# 
-#   # Evaluate the model and print results
-#   eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-#       x={"x": eval_data},
-#       y=eval_labels,
-#       num_epochs=1,
-#       shuffle=False)
-#   eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
-#   print(eval_results)
+# testing 100 images, with training data of ten randomly sampled images
+  result = []
+  for i in range(300,400):
+  	result.append(train_labels[i]==classify1and2(i, 10)[0])
+  	print(len(result))
+  	print(len([x for x in result if x == True])/len(result))
+  print(len([x for x in result if x == True])/len(result))
 
 
 if __name__ == "__main__":
